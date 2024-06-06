f6e6c6f29edacaa6a66743408e7770b656463606
simane <enamsiy9@gmail.com>
2024-06-06 18:41:55 +0200

    clear code

--- Calendrier/src/main/java/fr/dauphine/miageIf/Calendrier/CalendrierController.java
+++ Calendrier/src/main/java/fr/dauphine/miageIf/Calendrier/CalendrierController.java
@@ -59,6 +59,7 @@ public class CalendrierController {
     }
 
     // Supprimer un calendrier by id
+    //donc suppression de ce dernier dans le microservices planning
     @DeleteMapping("/id/{id}")
     public ResponseEntity<Void> deleteCalendrier(@PathVariable Long id) {
         Optional<Calendrier> optionalCalendrier = calendrierRepository.findById(id);
@@ -109,16 +110,22 @@ public class CalendrierController {
         return ResponseEntity.noContent().build();
     }
 
+
+    // cette methode converti le type String en type Date
     public static Date convertStringToDate(String dateString) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
         return formatter.parse(dateString);
     }
 
+
+    //renvoyer les calendrier a une date donnee
     @GetMapping("/date/{date}")
     public ResponseEntity<List<Calendrier>> getCalendrierByDate(@PathVariable String date) throws ParseException {
         return ResponseEntity.ok(calendrierRepository.findCalendrierByDate(convertStringToDate(date)));
     }
 
+    //nom des sports a une date donnee
+    //on fait donc appel au microservices sport
     @GetMapping("/nomSportByDate/{date}")
 
     public ResponseEntity<List<String>> getSportByDate(@PathVariable String date) throws ParseException {
@@ -144,8 +151,40 @@ public class CalendrierController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
+
+
+    //nom des sites a une date donnee
+    //on fait donc appel au microservices site
+    @GetMapping("/nomSiteByDate/{date}")
+
+    public ResponseEntity<List<String>> getSiteByDate(@PathVariable String date) throws ParseException {
+
+        try {
+            //ici list des calendrier a une date donnee
+            List<Calendrier> evenements = calendrierRepository.findCalendrierByDate(convertStringToDate(date));
+
+            //ici je veux les id des sites dans la liste preced
+            List<Long> sitesIds = new ArrayList<>();
+            for (Calendrier evenement : evenements) {
+                sitesIds.add(evenement.getIdSite());
+            }
+
+            // here je fais l appel à la methode qui appele le microservice site
+            List<String> nomsSites = new ArrayList<>();
+            for (Long siteId : sitesIds) {
+                String nomSite = getSiteNameBySiteId(siteId);
+                nomsSites.add(nomSite);
+            }
+            return ResponseEntity.ok(nomsSites);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    //renvoyer le/les site(s) en fonction de son id en path
+    //on fait donc appel au microservice site pour reuperer le nom
     @GetMapping("/nameSite/id/{id}")
-    public String getSiteName(@PathVariable Long id) {
+    public String getSiteNameBySiteId(@PathVariable Long id) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:8000/site/id/" + id;
 
@@ -161,6 +200,9 @@ public class CalendrierController {
         }
     }
 
+
+    //renvoyer le/les sport en fonction de son id en path
+    //on fait donc appel au microservice sport pour reuperer le nom
     @GetMapping("/nameSport/id/{id}")
     public String getSportsbyId(@PathVariable Long id) {
         RestTemplate restTemplate = new RestTemplate();
@@ -177,6 +219,11 @@ public class CalendrierController {
             return "Lancez le microservice Sport !";
         }
     }
+
+
+
+
+
 
 
     @GetMapping("/getSportsDansUnSiteDonne/{nomSite}")
@@ -225,7 +272,7 @@ public class CalendrierController {
     }
 
 
-    @GetMapping("/sportDansSiteDonne/{nomSport}")
+    @GetMapping("/siteAvecSiteDonne/{nomSport}")
     public List<String> getSitesNameAvecSportDonne(@PathVariable String nomSport) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:8001/sport/name/" + nomSport;
@@ -236,11 +283,9 @@ public class CalendrierController {
                 Long idSportRecupere = Long.valueOf(response.get("id").toString());
                 System.out.println("idRecu : " + idSportRecupere);
                 List<Calendrier> list = calendrierRepository.findAllByIdSport(idSportRecupere);
-//                System.out.println(list.get(1).toString());
-//                System.out.println(list.get(2).toString());
                 for (Calendrier c : list) {
                     System.out.println(c.getIdSport());
-                    sites.add(this.getSiteName(c.getIdSport()));
+                    sites.add(this.getSiteNameBySiteId(c.getIdSport()));
                 }
                 System.out.println(sites);
                 return sites;
